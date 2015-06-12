6518bea6b7ce9da659e50cb3ec6a43885ab7b04d
Antonio <manentea@gmail.com>
2015-06-12 17:05:03 -0400

    added session controller and testing

--- Whack/spec/controllers/sessions_controller_spec.rb
+++ Whack/spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe SessionsController do
-
+  let!(:user) {User.create(name: 'Antonio', email: 'antonio@gmail.com', password: '12345')}
   context 'sessions#new' do
 
     it 'returns a successful response' do
@@ -16,6 +16,35 @@ describe SessionsController do
   end
 
   context 'sessions#create' do
-    it 'should '
+    it 'creates a new session with the correct user id' do
+      expect {
+        post :create, session: { email: 'antonio@gmail.com', password: '12345' }
+      }.to change { session[:user_id] }.to(user.id)
+    end
+
+    it 'redirects to user show page if authenticated' do
+      expect {
+        post :create, session: { email: 'antonio@gmail.com', password: '12345' }
+        }.to change { session[:user_id] }.to(user.id)
+        expect(response).to be_redirect
+    end
+  end
+
+
+  context 'sessions#delete' do
+    it 'destroys the session by resetting the session' do
+      session[:user_id] = 2
+      expect {
+        delete :destroy
+      }.to change { session[:user_id] }.to(nil)
+    end
+
+    it 'response is a redirect' do
+      session[:user_id] = 2
+      expect {
+        delete :destroy
+      }.to change { session[:user_id] }.to(nil)
+      expect(response).to be_redirect
+    end
   end
 end
