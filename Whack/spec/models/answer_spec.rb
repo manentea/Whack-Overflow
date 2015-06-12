1b70a00e4e81153f823b6fc224345facb058c762
Nikolai Calder <redlacialokin@gmail.com>
2015-06-12 14:52:31 -0400

    added test to check for valid attribute

--- Whack/spec/models/answer_spec.rb
+++ Whack/spec/models/answer_spec.rb
@@ -1,6 +1,4 @@
-require 'rails
-
-_helper'
+require 'rails_helper'
 describe Answer do
   context "validations" do
     it { should validate_presence_of :body }
@@ -12,4 +10,11 @@ describe Answer do
       }.not_to change { Answer.count }
     end
   end
+
+  it "is valid with a body" do
+    answer = Answer.new( body: "not blank")
+    expect(answer).to be_valid
+  end
+
+
 end
