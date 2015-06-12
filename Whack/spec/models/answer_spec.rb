9eb0b25a8099ac20bc44ccafca1d686aa80e2c88
Nikolai Calder <redlacialokin@gmail.com>
2015-06-12 14:31:12 -0400

    added test for validation, body can't be blank

--- Whack/spec/models/answer_spec.rb
+++ Whack/spec/models/answer_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require 'rails
 
-RSpec.describe Answer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+_helper'
+describe Answer do
+  context "validations" do
+    it { should validate_presence_of :body }
+    it "body must be present" do
+      expect {
+        answer = Answer.new
+        answer.save
+        expect(answer.errors.full_messages).to include("Body can't be blank")
+      }.not_to change { Answer.count }
+    end
+  end
 end
