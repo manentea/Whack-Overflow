b6351fb7a639493c91e8d1b30fe03890adeb6037
Nikolai Calder <redlacialokin@gmail.com>
2015-06-14 13:28:00 -0400

    added logic inside controller actions

--- Whack/app/controllers/answers_controller.rb
+++ Whack/app/controllers/answers_controller.rb
@@ -8,20 +8,30 @@ class AnswersController < ApplicationController
     @answer = Answer.find(params[:id])
   end
 
-  def create
+  def created
   end
 
   def update
+    @answer = Answer.find(params[:id])
+    @answer.update(answer_params)
+    if @answer.save
+       redirect_to answer_path(@answer)
+    else
+      render :edit
+    end
   end
 
   def edit
+    @answer = Answer.find(params[:id])
   end
 
   def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    redirect_to answer_path(@answer)
   end
 
   private
-
   def article_params
     params.require(:answer).permit(:body)
   end
